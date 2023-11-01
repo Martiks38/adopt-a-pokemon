76d2befe631a2c3efafb1c6066becedfaa068e42
Martiks38 <cappa.giuliano@gmail.com>
2023-11-01 16:12:56 -0300

    modify app-component and add header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./modules').then((m) => m.LoginModule),
+  },
+  {
+    path: 'signin',
+    loadChildren: () => import('./modules').then((m) => m.SigninModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
