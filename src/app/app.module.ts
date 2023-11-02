8e4e251baf7da3a3b4bc3804e06b255c90f2fc4c
Martiks38 <cappa.giuliano@gmail.com>
2023-11-02 17:35:42 -0300

    add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { HeaderComponent } from './shared';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, HeaderComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
