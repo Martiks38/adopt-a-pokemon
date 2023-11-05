8da489bdad12858e7edb316f94dfb8842f61d906
Martiks38 <cappa.giuliano@gmail.com>
2023-11-05 19:48:58 -0300

    create search-pokemon

---
+++ src/app/modules/home/components/search-pokemon/search-pokemon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-pokemon',
+  templateUrl: './search-pokemon.component.html',
+  styleUrls: ['./search-pokemon.component.scss'],
+  standalone: true,
+})
+export class SearchPokemonComponent {}
