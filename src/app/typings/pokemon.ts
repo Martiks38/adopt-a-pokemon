c5e27a240b84e46e933d35bb7b84e514e8f6bc5c
Martiks38 <cappa.giuliano@gmail.com>
2023-11-05 19:24:37 -0300

    modify pokemon data and create PokemonData

--- src/app/typings/pokemon.ts
+++ src/app/typings/pokemon.ts
@@ -10,15 +10,11 @@ interface PokemonURL {
   url: string;
 }
 
-interface Pokemon {
+interface MainPokemonData {
   name: string;
   height: number;
   weight: number;
   sprites: {
-    back_default: string | null;
-    back_female: string | null;
-    back_shiny: string | null;
-    back_shiny_female: string | null;
     front_default: string | null;
     front_female: string | null;
     front_shiny: string | null;
@@ -42,4 +38,15 @@ interface Pokemon {
   };
 }
 
-export { PokemonList, Pokemon };
+interface Pokemon extends MainPokemonData {
+  types: string[];
+}
+
+interface PokemonData extends MainPokemonData {
+  types: {
+    slot: number;
+    type: { name: string; url: string };
+  }[];
+}
+
+export { PokemonList, Pokemon, PokemonData };
