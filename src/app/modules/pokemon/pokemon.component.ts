5de1ccf942f9cbe0d5b92299750244a3456164cd
Martiks38 <cappa.giuliano@gmail.com>
2023-11-05 19:53:41 -0300

    create module pokemon
    Page module that shows the data of the pokémon to adopt

---
+++ src/app/modules/pokemon/pokemon.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { PokemonDataService } from 'src/app/services';
+import { storagePokemons } from 'src/assets/constants';
+import type { Pokemon } from 'src/app/typings/pokemon';
+import { capitalize } from 'src/app/utils/capitalize';
+
+@Component({
+  selector: 'app-pokemon',
+  templateUrl: './pokemon.component.html',
+  styleUrls: ['./pokemon.component.scss'],
+})
+export class PokemonComponent implements OnInit {
+  pokemon!: Pokemon;
+  error: { state: boolean; msg: string } = { state: false, msg: '' };
+
+  private readonly searchSvc = inject(PokemonDataService);
+
+  constructor(private router: ActivatedRoute, private titleSrv: Title) {}
+
+  ngOnInit(): void {
+    this.router.paramMap.subscribe((params) => {
+      const titleName = params.get('pokemon');
+
+      if (typeof titleName === 'string') {
+        this.titleSrv.setTitle(capitalize(titleName));
+
+        const pokemonsList = window.sessionStorage.getItem(storagePokemons);
+
+        if (typeof pokemonsList === 'string') {
+          const parsePokemonsList: Pokemon[] = JSON.parse(pokemonsList);
+
+          const indexPokemon = parsePokemonsList.findIndex(
+            ({ name }) => name === titleName
+          );
+
+          if (indexPokemon !== -1) {
+            this.pokemon = parsePokemonsList[indexPokemon];
+          } else {
+            this.getPokemon(titleName);
+          }
+        }
+      } else {
+        this.error = { state: true, msg: `${titleName} doesn't exist.` };
+      }
+    });
+  }
+
+  private getPokemon(name: string): void {
+    this.searchSvc
+      .getPokemon(name)
+      .pipe(
+        catchError((err: any) => {
+          if (err instanceof Error) {
+            this.error = { state: true, msg: err.message };
+          } else {
+            this.error = {
+              state: true,
+              msg: 'An error occurred while obtaining the Pokémon list.',
+            };
+          }
+          return throwError(
+            () => 'An error occurred while obtaining the Pokémon list.'
+          );
+        })
+      )
+      .subscribe((pokemon) => (this.pokemon = pokemon));
+  }
+}
---
+++ src/app/modules/pokemon/pokemon.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonComponent } from './pokemon.component';
+import { PokemonRoutingModule } from './pokemon-router.module';
+import { CapitalizePipe } from 'src/app/pipes/capitalize.pipe';
+import { ListFormPipe } from 'src/app/pipes/listForm.pipe'
+
+@NgModule({
+  declarations: [PokemonComponent, CapitalizePipe, ListFormPipe],
+  imports: [CommonModule, PokemonRoutingModule],
+})
+export class PokemonModule {}
