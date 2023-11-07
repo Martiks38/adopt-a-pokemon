6fbd2224c70b181a9a3a0cc4bc6edc62ebfc128a
Martiks38 <cappa.giuliano@gmail.com>
2023-11-07 16:53:50 -0300

    add page title handling
    when changing the title of the page when browsing, it keeps this one and not the one at the beginning

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(private titleSrv: Title) {}
+
+  ngOnInit(): void {
+    this.titleSrv.setTitle('Adopt a Pokemon');
+  }
+}
