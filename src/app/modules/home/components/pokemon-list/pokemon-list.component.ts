6671c996d83a140f7b85d89d02b936593d4ddafc
Martiks38 <cappa.giuliano@gmail.com>
2023-11-21 17:43:59 -0300

    modify the pokemon-list template

--- src/app/modules/home/components/pokemon-list/pokemon-list.component.ts
+++ src/app/modules/home/components/pokemon-list/pokemon-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Pokemon } from 'src/app/typings/pokemon';
 import { LoaderComponent } from 'src/app/shared';
 import { PokemonItemComponent } from '../pokemon-item';
+import { Pokemon } from 'src/app/typings/pokemon';
 
 @Component({
   selector: 'app-pokemon-list',
