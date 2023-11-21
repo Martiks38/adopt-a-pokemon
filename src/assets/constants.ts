cb2818d06356b0c5adb61c34aa728133b8112840
Martiks38 <cappa.giuliano@gmail.com>
2023-11-21 17:59:54 -0300

    add new constants

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -1,2 +1,5 @@
 export const storagePokemons = 'session-pokemons';
 export const storagePokemon = 'session-pokemon';
+export const storageOffsetPokemonList = 'session-offsetPokemonsList';
+export const getLimitPokemons = 10;
+export const minCharactersPassword = 6;
