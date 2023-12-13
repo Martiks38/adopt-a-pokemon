fff6db216f5ef0c6698067ad5449503d572f6ed7
Martiks38 <cappa.giuliano@gmail.com>
2023-12-13 18:21:38 -0300

    add a list of mocked users

---
+++ src/assets/db/users.ts
@@ -0,0 +1,7 @@
+import { UserInformation } from 'src/app/typings/user';
+
+export const USERS: UserInformation[] = [
+  { email: 'example@gmail.com', password: '123456' },
+  { email: 'manuel@gmail.com', password: '123456' },
+  { email: 'alejandra@gmail.com', password: '123456' },
+];
