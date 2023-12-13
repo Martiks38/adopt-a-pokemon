902f19412e6eddd5efe1422bf0a789df88c68254
Martiks38 <cappa.giuliano@gmail.com>
2023-12-13 18:33:52 -0300

    modify the page header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectQuantityPokemons } from '../state';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +12,17 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  quantity: number = 0;
+  pokemonStore$: Observable<number> = new Observable();
+
+  constructor(private store: Store) {
+    this.pokemonStore$ = this.store.select(selectQuantityPokemons);
+  }
+
+  ngOnInit(): void {
+    this.pokemonStore$.subscribe(
+      (quantityOfPokemons) => (this.quantity = quantityOfPokemons)
+    );
+  }
+}
